df779118e0ce6c4314413e1e65d57ba7884691bf
xie-chun-hui <1194620498@qq.com>
2021-06-11 20:43:14 +0800

    complete single machine concurrent cache module.

---
+++ byteview.go
@@ -0,0 +1,23 @@
+package GoCache
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(bytes []byte) []byte {
+	c := make([]byte, len(bytes))
+	copy(c, bytes)
+	return c
+}
